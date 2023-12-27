32e27fb8aab8b3e9564725adef8ed837d390fbe4
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-12-28 01:18:38 +0600

    Radix UI added

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@radix-ui/themes';
+import React from 'react';
+
+
+const page = () => {
+  return (
+    <Button>New Issue</Button>
+  )
+}
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './Navbar'
-import Footer  from './Footer'
+import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Issue Tracker',
-  description: 'My first Next and Tyscript App',
-}
+  title: "Issue Tracker",
+  description: "My first Next and Tyscript App",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar/>
-        <main>
-        {children}
-        </main>
-        <Footer/>
-        </body>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
